24e0348d9252b4e21441497631dd8612d1e28c0f
Murillo Kauê Ribeiro <84983717+kaeuem22@users.noreply.github.com>
2022-06-06 11:29:06 -0300

    Add files via upload

--- Rating-new/scripts/index.js
+++ Rating-new/scripts/index.js
@@ -1,27 +1,40 @@
 let selectedOption = 0;
 
-function validate(event){
 
-    if(selectedOption === 0){
-        event.preventDefault()// estamos usando um event para evitar que a página recarregue (preventDefault), o evento é recebido de todos os botões
-        alert("please select an option!")
-        return
+// ---------------------------------VOTO-------------------------
+const vote = {
+    validate(event){
+
+        if(selectedOption === 0){
+            event.preventDefault()// estamos usando um event para evitar que a página recarregue (preventDefault), o evento é recebido de todos os botões
+            alert("please select an option!")
+            return
+        }
+    
+        localStorage.setItem('selectedOption',selectedOption)//o set iten recebe 2 parametros, o nome do campo em string e o valor a ser salvo
+    },
+
+    changeSelectedOption(optionNumber){
+        selectedOption = optionNumber
     }
-
-    localStorage.setItem('selectedOption',selectedOption)//o set iten recebe 2 parametros, o nome do campo em string e o valor a ser salvo
 }
 
-function start(){
-    const options = document.querySelectorAll('.options button')
-    const button = document.querySelector('#submit-button')
 
-    options.forEach(option => {
-        option.addEventListener('click', () => {
-            selectedOption = parseInt(option.textContent)
-        })
-    })//usando arrow functions
-
-    button.addEventListener('click', validate)
+// ---------------------INICIO DA APLICAÇÂO-------------------------
+const app = { // objeto com funções
+    start(){
+        const options = document.querySelectorAll('.options button')
+        const button = document.querySelector('#submit-button')
+    
+        options.forEach(option => {
+            option.addEventListener('click', () => {
+                let optionNumber = parseInt(option.textContent)
+                vote.changeSelectedOption(optionNumber)
+            })
+        })//usando arrow functions
+    
+        button.addEventListener('click', vote.validate)
+    }
 }
 
-start ()
+app.start()
